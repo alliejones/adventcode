ab5080652ad7cf4d5c8f309466ab92d3e96edd14
Allie Jones <allie@alliejon.es>
2015-12-02 16:39:33 -0500

    Day 2

---
+++ 2.1.py
@@ -0,0 +1,13 @@
+total_area = 0
+
+with open('2-input.txt') as file:
+    for line in file:
+        sides = map(int, line.split('x'))
+        w, h, l = sides
+        area = 2*l*w + 2*w*h + 2*h*l
+
+        sides.remove(max(*sides))
+        area += sides[0] * sides[1]
+        total_area += area
+
+print total_area
---
+++ 2.2.py
@@ -0,0 +1,14 @@
+total_length = 0
+
+with open('2-input.txt') as file:
+    for line in file:
+        sides = map(int, line.split('x'))
+        w, h, l = sides
+
+        volume = w * h *l
+        total_length += volume
+
+        sides.remove(max(*sides))
+        total_length += sides[0]*2 + sides[1]*2
+
+print total_length
