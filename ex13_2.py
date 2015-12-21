b29e12889a4da6546853c927d190d850af0faab1
Allie Jones <allie@alliejon.es>
2015-12-20 21:14:13 -0500

    Day 13

---
+++ ex13_2.py
@@ -0,0 +1,40 @@
+import re
+from itertools import permutations
+
+def read_happiness_vals():
+    vals = {}
+    with open('ex13-input.txt') as file:
+        for line in file:
+            matches = re.finditer('(\w+) would (gain|lose) (\d+) .* (\w+)\.$', line)
+            for match in matches:
+                p1, gain_lose, amt, p2 = match.groups()
+
+                if not p1 in vals:
+                    vals[p1] = {}
+
+                amt = int(amt)
+                if gain_lose == 'lose':
+                    amt = amt * -1
+
+                vals[p1][p2] = amt
+    return vals
+
+def calc_happiness_totals(happiness_vals):
+    happiness_totals = []
+    guests = happiness_vals.keys()
+    guests.append('me')
+    perms = permutations(guests)
+
+    for arrangement in perms:
+        total = 0
+        for i, p1 in enumerate(arrangement):
+            p2 = arrangement[i-1]
+            if p1 != 'me' and p2 != 'me':
+                total += happiness_vals[p1][p2]
+                total += happiness_vals[p2][p1]
+        happiness_totals.append(total)
+
+    return happiness_totals
+
+vals = calc_happiness_totals(read_happiness_vals())
+print max(vals)
